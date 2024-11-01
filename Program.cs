572d18f8e085ad3077a8246db483498c16eecb60
Dhanglerson Barral <dhanglerson.barral@crfsp.org.br>
2024-11-01 13:23:27 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using BarraldevDownloader.Components;
+//using BarraldevDownloader.Services; 
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+//builder.Services.AddSingleton<DownloadService>(); // Registro do serviço de download
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ Services/DownloadService.cs
@@ -0,0 +1,41 @@
+﻿//using System.IO;
+//using System.Threading.Tasks;
+//using YoutubeExplode;
+//using YoutubeExplode.Videos.Streams;
+
+//namespace BarraldevDownloader.Services
+//{
+//    public class DownloadService
+//    {
+//        private readonly YoutubeClient _youtubeClient;
+
+//        public DownloadService()
+//        {
+//            _youtubeClient = new YoutubeClient();
+//        }
+
+//        public async Task<MemoryStream> DownloadVideoAsync(string url)
+//        {
+//            var videoId = YoutubeClient.ParseVideoId(url);
+//            var streamManifest = await _youtubeClient.Videos.Streams.GetManifestAsync(videoId);
+//            var streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
+
+//            var memoryStream = new MemoryStream();
+//            await _youtubeClient.Videos.Streams.DownloadAsync(streamInfo, memoryStream);
+//            memoryStream.Position = 0;
+//            return memoryStream;
+//        }
+
+//        public async Task<MemoryStream> DownloadAudioAsync(string url)
+//        {
+//            var videoId = YoutubeClient.ParseVideoId(url);
+//            var streamManifest = await _youtubeClient.Videos.Streams.GetManifestAsync(videoId);
+//            var audioStreamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+
+//            var memoryStream = new MemoryStream();
+//            await _youtubeClient.Videos.Streams.DownloadAsync(audioStreamInfo, memoryStream);
+//            memoryStream.Position = 0;
+//            return memoryStream;
+//        }
+//    }
+//}
